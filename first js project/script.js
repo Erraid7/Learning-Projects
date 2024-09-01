46c1b2212d80ff0d7c142f4dcf339ca14b8bca5d
djemai erraid <nm_djemai@esi.dz>
2024-09-01 22:44:44 +0100

    first try to use json file (as a simple data-base)

--- first js project/script.js
+++ first js project/script.js
@@ -1,7 +1,17 @@
-//checking a task from the list
+
 const tasksList = document.getElementById("tasks-list");
 const notDoneTasks = document.getElementById("not-done-tasks");
 const doneTasks = document.getElementById("done-tasks");
+let tasks;
+
+fetch('./todos.json')
+  .then(response => response.text())
+  .then(data => {
+    tasks = JSON.parse(data);
+    console.log(tasks);
+    tasks.forEach(task => { addTask(task.text, task.completed); });
+  })
+  .catch(error => console.error(error));
 
 
 tasksList.addEventListener("click", function (e) {
@@ -24,16 +34,50 @@ tasksList.addEventListener("click", function (e) {
 
 
 //adding a new task to the list
-function addTask() {
-  const task = document.getElementById("task-input").value;
-  if (task === "") {
-    alert("Please enter a task");
-  } else {
-    const newTask = document.createElement("li");
-    newTask.innerHTML = `<button class="custom-checkbox-button"></button>
-    <span class="task">${task}</span>
-    <span class="delete-icon"></span>`;
-    notDoneTasks.appendChild(newTask);
-    document.getElementById("task-input").value = "";
+function addTask(task,status) {
+  const newTask = document.createElement("li");
+  newTask.innerHTML = `<button class="custom-checkbox-button"></button>
+  <span class="task">${task}</span>
+  <span class="delete-icon"></span>`;
+  if(status){
+    doneTasks.appendChild(newTask);
+    const checkboxButton = newTask.querySelector(".custom-checkbox-button");
+    checkboxButton.classList.add("checked");
   }
+  else {
+    notDoneTasks.appendChild(newTask);
+  }
+}
+
+// reading the task from the user input
+function readTask() {
+  const task = document.getElementById("task-input").value;
+  if (task) {
+    console.log(task);
+    addTask(task);
+    document.getElementById("task-input").value = "";
+    updateFile(task);
+  } else {
+    alert("Please enter a task");
+  }
+}
+
+//writing the new task to the file
+function updateFile(task) {
+  tasks.push({ text: task, completed: false });
+  const data = JSON.stringify(tasks);
+  fetch('./todos.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: data,
+  })
+    .then(response => response.text())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
